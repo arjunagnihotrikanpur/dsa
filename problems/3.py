f552daf47daf940966f61ee65cf0356845424293
Arjun Agnihotri <arjunagnihotrikanpur@gmail.com>
2026-03-23 23:32:00 +0530

    Check if a number is pallindrome or not - 23 March

--- problems/3.py
+++ problems/3.py
@@ -1,1 +1,17 @@
-# Problem 3
+# Problem 3 - Check if a number is pallindrome or not
+
+newNum = 0
+
+num = int(input("Enter a number: "))
+numCopy = num
+while (numCopy != 0):
+    lastDigit = numCopy % 10
+    newNum = (newNum * 10) + lastDigit 
+    numCopy = numCopy // 10
+
+print(newNum)
+if (num == newNum):
+    print("Pallindrome!")
+else:
+    print("Not a pallindrome!")
+    
