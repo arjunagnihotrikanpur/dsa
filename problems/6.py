07fe2ea854fb330ddfd884f7ea22f4193f9f692b
Arjun Agnihotri <arjunagnihotrikanpur@gmail.com>
2026-03-24 17:40:25 +0530

    Frequency Map - Approach 1 : 24 March

---
+++ problems/6.py
@@ -0,0 +1,14 @@
+# Frequency Map - Approach 1
+# TC: O(n)
+
+nums = [5,6,7,7,1,9,111,1,1,5,1,1]
+
+freq_map = {}
+
+for i in range(0, len(nums)):
+    if (nums[i] in freq_map):
+        freq_map[nums[i]] += 1
+    else:
+        freq_map[nums[i]] = 1
+        
+print(freq_map)
